392ab1837341d41c0f5373ddfdd6e5f530e00cef
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-09-27 09:13:23 -0400

    Create lock-box.js

---
+++ lock-box.js
@@ -0,0 +1,13 @@
+const promt = require('promt-sync')({sigint: true});
+let pinCoden = 1234;
+console.log = number("the pinCode has four digits");
+
+let userinput = number(prompt("please enter your pinCode: "));
+console.log(pinCode);
+
+if(userinput === pinCode){ 
+  ("console.log("success");
+  
+}  else{
+  console.log("failure")
+}                     
