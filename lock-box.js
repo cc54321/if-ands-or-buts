1c61f324fc0df6dddfb9bdd439306f88f11dfab0
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 17:56:48 -0400

    Update lock-box.js

--- lock-box.js
+++ lock-box.js
@@ -1,13 +1,30 @@
-const promt = require('promt-sync')({sigint: true});
-let pinCoden = 1234;
-console.log = number("the pinCode has four digits");
+/*lock-box.js
+You've been contracted to write the software for a lock box.
 
-let userinput = number(prompt("please enter your pinCode: "));
-console.log(pinCode);
+Write a program called lock-box.js that stores the pin code to the 
 
-if(userinput === pinCode){ 
-  ("console.log("success");
-  
-}  else{
-  console.log("failure")
-}                     
+1. lock box in a variable at the top of your program (i.e.: let pinCode = '1234';).
+
+2. After that, use prompt-sync to prompt the user for the passcode. 
+
+3.If the user gets the passcode exactly correct, log a success message containing the word, "success". 
+
+4.If not, log a failure message containing the word, "failure".
+start here !!!!
+Note: In this assignment, the node_modules folder containing the prompt-sync dependency does not already exist. To add it to this project, navigate to this repository using the command line then install it using the Node Package Manager with npm install prompt-sync.
+*/
+
+const prompt = require("prompt-sync")({sigint: true});
+let pinCode = 1234;
+console.log("The lock-box has four digits pin code");
+
+let userInput = Number(prompt("please enter your pin Code: "));
+
+if(userInput === pinCode){
+  console.log("Success!") 
+}
+
+else{
+  console.log("Failure")
+}         
+                  
