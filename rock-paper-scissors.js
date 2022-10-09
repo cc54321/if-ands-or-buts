6d9ad48fcecb23f0e9a8e2b92a48e64b7bb87682
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 20:19:43 -0400

    Update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,26 +1,50 @@
 
-const promt = require('promt-sync')({sigint: true});
+/*rock-paper-scissors.js
+Let's play a classic game of rock, paper, scissors!
 
-let Player1 = prompt("choose rock, paper, or scissors:")
-let Player2 = prompt("choose rock, paper, or scissors:")
+Write a program called 'rock-paper-scissors.js' that takes in two inputs in a row for Player 1 and Player 2.
 
-if ((Player1 === "rock" ||Player1 === "paper" || Player1 === "scissors")) && ((Player2 === "rock" ||Player2 === "paper" || Player2 === "scissors")) {
-    if (player1 === player2) {
-console.log('draw');
-    } else if (Player1 === "rock" && Player2 === "scissors"){
-    console.log("player 1  wins");
-} else if (Player1 === "paper" && Player2 === "rock"){
-    console.log("player 1  wins");
-} else if (Player1 === "scissors" && Player2 === "paper"){
-    console.log("player 1  wins");
-} else if (Player2 === "paper" && Player1 === "rock"){
-    console.log("player 2  wins");
-} else if (Player2 === "scissors" && Player1 === "paper"){
-    console.log("player 2  wins");
-} else if (Player1 === "paper" && Player1 === "rock"){
-    console.log("Player 2  wins");
-}
-} else {
-    console.log("errors invalid entry")
+2 inputs
 
-}
+The players can only input "rock", "paper", and "scissors". Anything else should result in a failure message getting logged to the screen. After both players input their options, use console.log() to print the player who emerged victorious.
+
+For those who are unfamiliar, rock beats scissors, scissors beats paper, and paper beats rock. If both players input the same answer, it should result in a draw.
+
+An example game
+
+Player 1, enter "rock", "paper", or "scissors":
+> rock
+Player 2, enter "rock", "paper", or "scissors":
+> paper
+Player 2 wins!
+
+*/
+
+const prompt = require('prompt-sync')({sigint: true});
+
+let player1 = prompt("Player 1: choose rock, paper, or scissors: ")
+let player2 = prompt("Player 2: choose rock, paper, or scissors: ")
+
+
+if ((player1 === "rock" || player1 === "paper" || player1 === "scissors") && ((player2 === "rock" || player2 === "paper" || player2 === "scissors")){
+
+ if (player1 === player2)
+    console.log(" draw! ");
+
+ }else if (player1 === "rock" && player2 === "scissors") {
+    console.log("player 1 wins");
+ }else if (player1 === "paper" && player2 === "rock") {
+    console.log("player 1 wins");
+ }else if (player1 === "scissors" && player2 === "paper") {
+    console.log("player 1 wins");
+ }else if (player2 === "paper" && player1 === "rock") {
+    console.log("player 2 wins");
+ }else if (player2 === "scissors" && player1 === "paper") {
+    console.log("player 2 wins");
+ }else if (player1 === "paper" && player1 === "rock") {
+    console.log("player 2 wins");
+ }else{
+console.log("error: invalid entry")
+ }
+
+
